af71ae19a1bcad420d894a11dcb0deb929587b6c
tbchk <fernando@guane.com.co>
2020-09-23 14:57:03 -0500

    added to interface

--- simplestorage/__init__.py
+++ simplestorage/__init__.py
@@ -1,3 +1,4 @@
+from io import BytesIO, StringIO
 from .interface import StorageInterface
 
 interface_session = StorageInterface()
@@ -9,3 +10,15 @@ def ls(uri: str):
 
 def save(data: bytes, uri: str):
     return interface_session.save(data=data, uri=uri)
+
+
+def load(data: bytes, uri: str) -> BytesIO:
+    return interface_session.load(uri=uri)
+
+
+def loads(self, uri: str) -> StringIO:
+    return interface_session.loads(uri=uri)
+
+
+def delete(self, uri: str) -> bool:
+    return interface_session.delete(uri=uri)
