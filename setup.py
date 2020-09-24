8fc9850602f4faf04aaf86749f9183021a75cc34
tbchk <fernando@guane.com.co>
2020-09-24 16:07:15 -0500

    0.2.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='simplestorage',
-    version='0.2.3',
+    version='0.2.4',
     description='Storage client for local and gcloud',
     url='https://github.com/tbchk/simple-storage',
     download_url='https://github.com/tbchk/simple-storage/archive/v0.2.3.tar.gz',
